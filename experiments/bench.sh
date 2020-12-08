5767e4e7a23c20a819c2a5e74d461924000f49e8
Zildj1an <bilbao@vt.edu>
2020-12-08 10:57:34 -0500

    Print time

--- experiments/bench.sh
+++ experiments/bench.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-make clean
-make
-
 echo "-----------------------------------------------"
 echo " LOG OF EXECUTION $(date +"%x %r") "
 echo "-----------------------------------------------"
 
 for i in {1..30}
 do
-	./test_prios >> log_20threads
-	./test_prios -f >> log_20threads
+	./test_prios
+	./test_prios -f
 done
 
 echo "-----------------------------------------------"
