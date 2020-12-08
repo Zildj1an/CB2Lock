1e3fdc6201e9d5535f3f35b64e695e2977da856c
Zildj1an <bilbao@vt.edu>
2020-12-07 21:27:57 -0500

    Logs

--- experiments/test_prios.c
+++ experiments/test_prios.c
@@ -31,13 +31,37 @@ struct test_run {
 };
 
 pthread_barrier_t barrier;
-pthread_mutex_t lock;
 int lowest_acquired = 0;
+
+pthread_spinlock_t spinlock;
+
+void timeval_substract(struct timespec *result, 
+		  struct timespec *x, 
+		  struct timespec *y)
+{
+	int nsec;
+	unsigned long long nano = 1000000000; 
+  	
+	if (x->tv_nsec < y->tv_nsec) {
+    		nsec = (y->tv_nsec - x->tv_nsec) / nano + 1;
+    		y->tv_nsec -= nano * nsec;
+    		y->tv_sec += nsec;
+  	}
+
+  	if (x->tv_nsec - y->tv_nsec > nano) {
+    		nsec = (x->tv_nsec - y->tv_nsec) / nano;
+    		y->tv_nsec += nano * nsec;
+    		y->tv_sec -= nsec;
+  	}
+
+  	result->tv_sec = x->tv_sec - y->tv_sec;
+  	result->tv_nsec = x->tv_nsec - y->tv_nsec;
+}
 
 void *thread_func(void *vargp) 
 {
 	struct test_run *tr = (struct test_run*)vargp;
-	struct timespec start;
+	struct timespec start, aux_time;
 	int rc, s = 0, m = 0;
 
 	if (setpriority(PRIO_PROCESS,getpid(),tr->priority) == -1 ){
@@ -53,20 +77,21 @@ void *thread_func(void *vargp)
 
 try_again:
 
-	/* Force the first thread (the lowest prio if flags not set) to acquire first */
+	/* Force the first thread (the lowest prio if !flags) acquire first  */
 	if (tr->id != 0 && !lowest_acquired){
 		asm("");
 		goto try_again;
 	}
 
 	/* Measure how long this thread has the lock */
-	pthread_mutex_lock(&lock);
+	 
+	pthread_spin_lock(&spinlock);
 
-	/* ##########################  CRITICAL SECTION ######################### */
+	/* #####################  CRITICAL SECTION ######################### */
 
-	clock_gettime(CLOCK_MONOTONIC, &start);
+	clock_gettime(CLOCK_THREAD_CPUTIME_ID, &start);
 
-	/* If we set this for every thread regardless, we don't need an atomic flag */
+	/* If we set this for every thread, we don't need an atomic flag     */
 	lowest_acquired = 1;
 
 	/* Let's make the time gap more obvious */
@@ -74,13 +99,28 @@ try_again:
 		asm(""); /* Avoids GCC optimizations */
 	}
 
-	pthread_mutex_unlock(&lock);
-	clock_gettime(CLOCK_MONOTONIC, &tr->tp);
+	pthread_spin_unlock(&spinlock);
+	clock_gettime(CLOCK_THREAD_CPUTIME_ID, &tr->tp);
 
-	tr->tp.tv_sec -= start.tv_sec;
-	tr->tp.tv_nsec -= start.tv_nsec;
+	timeval_substract(&aux_time,&tr->tp,&start);
+
+	if (aux_time.tv_sec > 0){
+		tr->tp.tv_sec  = aux_time.tv_sec;
+	}
+	else {
+		tr->tp.tv_sec = 0;
+	}
+
+	tr->tp.tv_nsec = aux_time.tv_nsec;
 
 	return (void*)tr;
+}
+
+int compute_percentage(struct test_run *tr, long long int total)
+{
+	long long int part, nano = 1000000000; 
+	part  = (tr->tp.tv_sec * nano) + tr->tp.tv_nsec;
+	return ((double)part / total) * 100;
 }
 
 int main(int argc, char *argv[])
@@ -88,9 +128,15 @@ int main(int argc, char *argv[])
 	int i, opt, cpu = 0, ncpu = get_nprocs(), thread_count = ncpu, flags = 0;
 	pthread_t *threads;
 	pthread_attr_t thread_attr;
-	struct test_run *tr;
+	struct test_run *tr, *collection_tr;
+	/* bench_time is for the execution of all threads and total_time
+           is the sum of per-thread CPU time.
+	*/
+	struct timespec start_bench, end_bench, total_time, bench_time;
 	cpu_set_t cpuset;
-
+        long long int total, nano = 1000000000;
+	char *sp1= "  ", sp2 = ' ';
+	
 	while ((opt = getopt(argc, argv, "hn:f")) != -1) {
 		switch (opt) {
 			case 'h':
@@ -139,17 +185,22 @@ int main(int argc, char *argv[])
 		errExit("Could not set the CFS scheduler");
 	}
 
-	/* Initialize mutex */
-	if (pthread_mutex_init(&lock, NULL) != 0) {
-		errExit("Mutex init");
-	}
+	/* Initialize spinlock */
+ 	pthread_spin_init(&spinlock, 0);
 
 	/* Initialize barrier */
 	if (pthread_barrier_init(&barrier, NULL, thread_count) != 0) {
 		errExit("Barrier init");
 	}
 
+	/* Allocate memory for the array of test run */
+	if (!(collection_tr = calloc(thread_count, sizeof(struct test_run)))){
+		errExit("Could not calloc collection_tr");
+	}
+
 	/* #### Create the threads and assign their priorities. ################# */
+
+	clock_gettime(CLOCK_MONOTONIC, &start_bench);
 
 	for (i = 0; i < thread_count; i++){
 
@@ -180,24 +231,53 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	/* ############ Join and process results ################################ */
+	/* ############ Join and process results ########################### */
+
+	total_time.tv_sec  = 0;
+	total_time.tv_nsec = 0;
 
 	for (i = 0; i < thread_count; ++i) {
 
 		if (pthread_join(threads[i], (void**)&tr) != 0) {
 			errExit("Could not join thread");
 		}
+		
+		total_time.tv_nsec  += tr->tp.tv_nsec;	
+		total_time.tv_sec  += tr->tp.tv_sec;
 
-		printf("Priority of thread %d: %d\t\t time: %d:%d\n", i,
-			     tr->priority, tr->tp.tv_sec, tr->tp.tv_nsec);
-		free(tr);
+		collection_tr[i].tp.tv_sec  = tr->tp.tv_sec;
+		collection_tr[i].tp.tv_nsec = tr->tp.tv_nsec;
+		collection_tr[i].priority   = tr->priority;
 	}
 
-	/* Cleanup */
-	pthread_mutex_destroy(&lock); 
-	pthread_barrier_destroy(&barrier);
+	total = total_time.tv_sec * nano + total_time.tv_nsec; 
+	
+	for (i = 0; i < thread_count; ++i){
 
+		tr = &collection_tr[i];
+
+		printf("Thread %d: Priority %s%d\t CPU time: %d:%09d\t CPU%: %d\n",
+		       i,(tr->priority > 0)?sp1:&sp2,
+		       tr->priority, tr->tp.tv_sec, 
+		       tr->tp.tv_nsec, compute_percentage(tr,total));
+	}
+
+	/* Compute total execution time */
+	clock_gettime(CLOCK_MONOTONIC, &end_bench);
+	
+	timeval_substract(&bench_time,&end_bench,&start_bench);
+
+	printf("Total clock time: %d:%09d\n",
+		(long long)bench_time.tv_sec,bench_time.tv_nsec);
+
+	printf("Total threads CPU time: %d:%09d\n",
+		(long long)total_time.tv_sec,total_time.tv_nsec);
+	
+	/* Cleanup */
+	pthread_spin_destroy(&spinlock);
+	pthread_barrier_destroy(&barrier);
 	free(threads);
+	free(collection_tr);
 	pthread_attr_destroy(&thread_attr);
 
 	exit(EXIT_SUCCESS);
