9c02d980d2a98e36eb68dbf39f106e5ebb36ce80
Zildj1an <bilbao@vt.edu>
2020-12-08 12:10:42 -0500

    experiments

--- experiments/bench.sh
+++ experiments/bench.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# Testing of the microbenchmark 
 
 echo "-----------------------------------------------"
 echo " LOG OF EXECUTION $(date +"%x %r") "
 echo "-----------------------------------------------"
+
+# Disable hyperthreading
+./hyperthreading.sh &> /dev/null
 
 for m in {1..100}
 do
