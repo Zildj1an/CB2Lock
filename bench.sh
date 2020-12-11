38581c1ec4e0512347980ea4e5c0d2abe3e0acd4
Zildj1an <bilbao@vt.edu>
2020-12-10 22:20:42 -0500

    bench

--- bench.sh
+++ bench.sh
@@ -26,7 +26,14 @@ function disable_hyp {
 cowsay -f tux 'CB2Lock microbenchmark '$(date +"%x %r") | toilet --metal -f term 
 
 # Disable hyperthreading
-disable_hyp &> /dev/null
+#disable_hyp &> /dev/null
+
+if [ $# -eq 1 ]; then
+	if test -f $1; then
+   		echo "Parsing configuration file..."
+		echo $1
+	fi
+fi
 
 cd src
 
