47e916509e00b1b3c1303e24a91239010283b90a
Carlos Bilbao <bilbao@vt.edu>
2020-12-12 13:47:27 -0500

    Need slowdown

--- experiments/slowdown_by/slowdown.sh
+++ experiments/slowdown_by/slowdown.sh
@@ -3,7 +3,7 @@
 declare -i sum=0
 declare -i time=0
 
-for i in {1..100000}
+for i in {1..10000}
 do
 	time=$( ./by ) 
 	sum=$(($sum + $time))
@@ -12,5 +12,5 @@ done
 declare -i average=0
 
 echo "Total CPU time: " $sum
-average=$(($sum / 100000))
+average=$(($sum / 10000))
 echo "Average CPU time (100 thousand executions):" $average
